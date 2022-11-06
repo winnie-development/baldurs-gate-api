6ccec9a8876b62ee30a4448bd784e6036162abd2
Winnie <winnie.yip@3plearning.com>
2022-11-06 10:39:55 +1000

    build: Add HTTPS redirection

--- Startup.cs
+++ Startup.cs
@@ -1,7 +1,7 @@
+using System.Net;
 using BaldursGateAPI.Helpers;
 using BaldursGateAPI.Services.Alignment;
 using BaldursGateAPI.Services.Character;
-using BaldursGateAPI.Services.Class;
 using BaldursGateAPI.Services.Class;
 using BaldursGateAPI.Services.Race;
 using Microsoft.EntityFrameworkCore;
@@ -15,11 +15,20 @@ builder.Services.AddScoped<IRaceService, RaceService>();
 builder.Services.AddScoped<IClassService, ClassService>();
 builder.Services.AddScoped<ICharacterService, CharacterService>();
 
-builder.Services.AddAWSLambdaHosting(LambdaEventSource.RestApi);
+builder.Services.AddAWSLambdaHosting(LambdaEventSource.HttpApi);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+if (!builder.Environment.IsDevelopment())
+{
+    builder.Services.AddHttpsRedirection(options =>
+    {
+        options.RedirectStatusCode = (int)HttpStatusCode.PermanentRedirect;
+        options.HttpsPort = 443;
+    });
+}
 builder.Services.Configure<RouteOptions>(options => options.LowercaseUrls = true); 
 
 builder.Services.AddDbContext<DataContext>(options => options.UseNpgsql(connectionString));
@@ -29,7 +38,7 @@ var app = builder.Build();
 app.UseSwagger(); 
 app.UseSwaggerUI();
 
-
+app.UseHsts();
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
